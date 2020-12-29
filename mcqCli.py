3305b6894235e742a0bf373ad895770252366dbe
jdswan <jeffdswan@outlook.com>
2020-12-29 00:40:51 -0600

    Adding review functionality to provide feedback as questions are answered.

--- mcqCli.py
+++ mcqCli.py
@@ -18,10 +18,31 @@ class Sprint:
             print(q["text"])
             for count, answer in enumerate(q["answers"]):
                 print(f"{self.reverse_lookup[count - 1]}. {answer[1]}")
-            resp = self.parse_answer(input())
+            response = self.keyed_response(len(q["answers"]), self.parse_answer(input()))
+            self.review(response, q["answers"])
             self.current += 1
 
     def parse_answer(self, answer: str):
         """Returns a list of selected answer choice indices."""
         labels = list(set([label.upper() for label in answer if label.upper() in "ABCDEF"]))
+        labels.sort()
         return [self.lookup[label] for label in labels]
+
+    def keyed_response(self, choice_count, selections):
+        response = [0 for i in range(choice_count)]
+        for i in selections:
+            response[i] = 1
+        return response
+
+    def review(self, response, answers):
+        if response == [a[0] for a in answers]:
+            print("Correct.")
+        else:
+            for i in range(len(answers)):
+                if answers[i][0]:
+                    if response[i]:
+                        print(f"{self.reverse_lookup[i]} is correct.")
+                    else:
+                        print(f"{self.reverse_lookup[i]} should have been selected.")
+                elif not answers[i][0] and response[i]:
+                    print(f"{self.reverse_lookup[i]} is not correct.")
