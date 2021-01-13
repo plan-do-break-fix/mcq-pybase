c324bc8ed10fff27541908716a3b8e3af3551d5e
jdswan <jeffdswan@outlook.com>
2021-01-12 23:16:36 -0600

    Refactored to take hash of string representation of entire question dict as question ID instead of just the question text. -The same question text with different answer choices are different  questions.

--- qsetMaker.py
+++ qsetMaker.py
@@ -1,20 +1,22 @@
 import hashlib, json
 from typing import List
 
+
 def make_qset(questions: List[dict]) -> dict:
-    return {hashlib.md5(q["text"].encode()).hexdigest(): q for q in questions if validate(q)}
+    return {hashlib.md5(json.dumps(q).encode()).hexdigest(): q for q in questions if validate(q)}
 
 
 def update_qset(qset: dict, questions: List[dict]) -> dict:
     for q in [_q for _q in questions if validate(_q)]:
-        q_id = hashlib.md5(q["text"].encode()).hexdigest()
+        q_id = hashlib.md5(json.dumps(q).encode()).hexdigest()
         if q_id not in qset.keys():
             qset[q_id] = q
     return qset
 
 
-def write_qset(qset: dict) -> None:
-    pass
+def write_qset(qset: dict, fname: str) -> None:
+    with open(f"{fname}.mcquiz", "w") as _f:
+        json.dump(qset, _f)
 
 
 def validate(question: dict) -> bool:
